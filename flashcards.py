697d2f8eb18586e9681221bddfee0160189c1e3d
Mohamed-98 <m.gado86@gmail.com>
2021-05-19 22:27:24 +0200

    as

---
+++ flashcards.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, abort
+from model import db
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def welcome():
+    return render_template(
+        "welcome.html",
+        x="this is test 1 for jinja"
+    )
+
+
+@app.route("/card/<int:index>")
+def card_view(index):
+    try:
+        card = db[index]
+        return render_template("card.html", card=card)
+    except IndexError:
+        abort(404)
